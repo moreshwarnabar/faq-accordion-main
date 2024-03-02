78606fff4c5f67a8f26fb3bf84103e0518563462
moreshwarnabar <mrnabar@gmail.com>
2024-03-01 18:34:46 -0700

    changed accordion style

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// click handlers
+function handleClick(e) {
+  if (e.srcElement.classList[0] == "content-title") return;
+
+  this.nextElementSibling.classList.toggle("active");
+  console.log(this.nextElementSibling);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("loaded");
+  const openers = [];
+  openers.push(...document.querySelectorAll(".content-title"));
+
+  openers.forEach((opener) => {
+    opener.addEventListener("click", handleClick);
+  });
 });

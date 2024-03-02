014686afcf466c52666c71625051e9f71b075609
moreshwarnabar <mrnabar@gmail.com>
2024-03-01 19:56:12 -0700

    added responsiveness for smaller screens

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+let prevThis;
+
 // click handlers
 function handleClick(e) {
   if (e.srcElement.classList[0] == "content-title") return;
+
+  if (
+    prevThis &&
+    prevThis.nextElementSibling != this.nextElementSibling &&
+    prevThis.nextElementSibling.classList.contains("active")
+  ) {
+    prevThis.nextElementSibling.classList.remove("active");
+    prevThis.children[1].classList.remove("hidden");
+    prevThis.children[2].classList.add("hidden");
+  }
 
   this.nextElementSibling.classList.toggle("active");
 
   this.children[1].classList.toggle("hidden");
   this.children[2].classList.toggle("hidden");
+
+  prevThis = this;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
